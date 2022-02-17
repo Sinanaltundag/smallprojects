73f3c70acb208cd016777f5a01a344354dcd4a8e
peykani <peykani@gmail.com>
2022-02-17 18:12:30 +0300

    completed roughly

--- js-projects/calculator/app.js
+++ js-projects/calculator/app.js
@@ -1,77 +1,86 @@
 let operateLine = document.getElementById("op-num");
 let resultLine = document.getElementById("result");
 let opSpan = document.getElementById("op");
-let percent = document.getElementById("percent");
-let allClear = document.getElementById("ac");
-let backspace = document.getElementById("del");
-let divide = document.getElementById("divide");
-let multiply = document.getElementById("multiply");
-let add = document.getElementById("add");
-let minus = document.getElementById("minus");
-let equal = document.getElementById("equal");
+let calculator = document.getElementById("container");
 let numbers = document.querySelectorAll(".numbers");
+console.log(eval("-5-3")); 
 
-let numset = [2, "+", 5];
-let num1, num2, operator;
-let operateInput = "";
-numbers.forEach((numButton) =>
-  numButton.addEventListener("click", () => {
-    resultLine.innerHTML += numButton.innerHTML;
-  })
-);
 
-// console.log(operate(2,"+",3));
-function operate(num1, num2, operator) {
-  if (num2) {
-    operateLine.innerHTML = eval(num1 + operator + num2);
+
+function operate() {
+  if (resultLine.innerHTML == "") {
+    return;
   }
-
+if (opSpan.innerHTML=="" && operateLine.innerHTML){
+         operateLine.innerHTML=resultLine.innerHTML;
+      resultLine.innerHTML=""
+      return
+    }
+  if (operateLine.innerHTML) {
+    if ((opSpan.innerHTML == "％")) {
+      operateLine.innerHTML =
+        (operateLine.innerHTML * resultLine.innerHTML) / 100;
+      resultLine.innerHTML = "";
+      return;
+    }
+    operateLine.innerHTML = eval(
+      operateLine.innerHTML + opSpan.innerHTML + resultLine.innerHTML
+    );
+    resultLine.innerHTML = "";
+    return;
+  }
   operateLine.innerHTML = resultLine.innerHTML;
-  opSpan.innerHTML = operator;
   resultLine.innerHTML = "";
 }
-divide.addEventListener("click", () => {
-  num1
-    ? (num2 = (resultLine.innerHTML, (num1 = operateLine.innerHTML)))
-    : (num1 = resultLine.innerHTML);
-  operate(num1, num2, "/");
-  num1 = operateLine.innerHTML;
-});
 
-multiply.addEventListener("click", () => {
-  console.log(num1);
-  if (num1) {
-    num2 = resultLine.innerHTML;
-    console.log(+num2);
-    resultLine.innerHTML = +num1 * +num2;
-    opSpan.innerHTML = `<i class="fas fa-solid fa-divide"></i>`;
+calculator.addEventListener("click", (e) => {
+  console.log(e);
+  switch (e.target.id) {
+    case "divide":
+    case "multiply":
+    case "add":
+    case "minus":
+    case "percent":
+      if (resultLine.innerHTML){
+        operate();
+      }
+      
+      if (operateLine.innerHTML){
+        opSpan.innerHTML = e.target.innerHTML;
+      }
+      break;
+    case "n1":
+    case "n2":
+    case "n3":
+    case "n4":
+    case "n5":
+    case "n6":
+    case "n7":
+    case "n8":
+    case "n9":
+    case "n0":
+      resultLine.innerHTML += e.target.innerHTML;
+      break;
+    case "equal":
+      operate();
+      opSpan.innerHTML = "";
+      break;
+    case "ac":
+      resultLine.innerHTML = opSpan.innerHTML = operateLine.innerHTML = "";
+      break;
+    case "del":
+      if (resultLine.innerHTML) {
+        resultLine.innerHTML = resultLine.innerHTML.slice(0, -1);
+      }
+      break;
+    case "dot":
+      if (resultLine.innerHTML && !resultLine.innerHTML.includes(".")) {
+        resultLine.innerHTML += ".";
+      }
+      break
+    default:
+      resultLine.innerHTML += e.target.innerHTML;
+      break;
   }
-  num1 = resultLine.innerHTML;
-  console.log(num1);
-  operateLine.innerHTML = resultLine.innerHTML;
-  opSpan.innerHTML = `<i class="fas fa-solid fa-asterisk"></i>`;
-  resultLine.innerHTML = operateInput = "";
 });
 
-equal.addEventListener("click", () => {
-  // for (let index = 0; index < numset.length; index++) {
-  //     let result = numset[index];
-  // }
-  // let result= +(numset.join(""))
-  // console.log(result);
-  // resultLine.innerHTML= 2/1+3*2;
-});
-
-allClear.addEventListener("click", () => {
-  resultLine.innerHTML =
-    opSpan.innerHTML =
-    operateLine.innerHTML =
-    num1 =
-    num2 =
-      "";
-});
-backspace.addEventListener("click", () => {
-  if (resultLine.innerHTML) {
-    resultLine.innerHTML = resultLine.innerHTML.slice(0, -1);
-  }
-});
