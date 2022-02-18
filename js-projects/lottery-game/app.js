11b6837562cd5d527314cc8f4a3eaccdc46d9b9a
peykani <peykani@gmail.com>
2022-02-18 14:26:01 +0300

    lottery game completed

---
+++ js-projects/lottery-game/app.js
@@ -0,0 +1,47 @@
+let inputNum = document.getElementById("input-num");
+let button = document.getElementById("button");
+let listBox = document.querySelector(".list-box");
+
+button.addEventListener("click", () => {
+  while (listBox.hasChildNodes()) {
+    listBox.removeChild(listBox.firstChild);
+  }
+  let n = inputNum.value;
+  if (n < 1 || n > 8 || n == "") {
+    alert("Lütfen 1 ile 8 arasında bir sayı giriniz.");
+    return;
+  }
+
+  for (let i = 0; i < n; i++) {
+    let rand = Math.floor(Math.random() * 100)+1;
+    let luckyNumberList = [];
+
+    while (luckyNumberList.length < 6) {
+      rand = Math.floor(Math.random() * 100 )+ 1;
+      if (!luckyNumberList.includes(rand)) {
+        luckyNumberList.push(rand);
+      }
+    }
+    luckyNumberList.sort(function (a, b) {
+      return a - b;
+    });
+    while (luckyNumberList.length < 7) {
+      rand = Math.floor(Math.random() * 100 )+ 1;
+      if (!luckyNumberList.includes(rand)) {
+        luckyNumberList.push(rand);
+      }
+    }
+    rand = Math.floor(Math.random() * 100) + 1;
+    luckyNumberList.push(rand);
+    let list = document.createElement("p");
+    list.innerText =
+      luckyNumberList.slice(0, 6).join(" , ") +
+      " | " +
+      luckyNumberList[6] +
+      " | **" +
+      luckyNumberList[7] +
+      "**";
+    listBox.appendChild(list);
+  }
+});
+
