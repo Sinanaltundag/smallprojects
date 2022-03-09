0c87efc49a6a8129d948c466c67f8ebc281d1378
peykani <peykani@gmail.com>
2022-03-09 19:11:41 +0300

    clock

--- js-projects/responsive-digital-clock/app.js
+++ js-projects/responsive-digital-clock/app.js
@@ -1,12 +1,12 @@
 const dateBox = document.querySelector(".date-box")
 const clockBox= document.querySelector(".time-box")
 
-let dateNow = new Date();
 
 
 
 setInterval(()=>{
-    dateNow = new Date();
+    let dateNow = new Date();
+   
 dateBox.textContent= dateNow.toLocaleDateString('tr-TR',{ weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
 
 clockBox.textContent= dateNow.toLocaleTimeString('en-US', {hour:"numeric", minute:"numeric", second:"numeric"})
